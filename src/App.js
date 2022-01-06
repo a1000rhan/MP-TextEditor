dca277f0575750eaffe3e41cfb574a091d07f476
a1000rhan <1000rhan@gmail.com>
2022-01-06 15:03:01 +0300

    almost done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState } from "react";
 
 const styles = {
   bold: { fontWeight: "bold" },
@@ -10,9 +11,18 @@ const stylings = ["bold", "italic", "underline"];
 
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
+const colorChange = (color) => {
+  console.log(color);
+};
 function App() {
+  const [changeValue, setChangeValue] = useState("");
   const stylingBoxes = stylings.map((style) => (
-    <button className="btn btn-light" style={styles[style]} key={style}>
+    <button
+      className="btn btn-light"
+      style={styles[style]}
+      key={style}
+      onClick={() => setChangeValue(style)}
+    >
       {style}
     </button>
   ));
@@ -21,13 +31,15 @@ function App() {
     <button
       style={{ backgroundColor: color, height: 30, width: 30 }}
       key={color}
+      onClick={() => setChangeValue((styles.color = { color: color }))}
     />
   ));
+  console.log(styles);
 
   return (
     <div className="App">
       <div className="my-3">{stylingBoxes}</div>
-      <textarea />
+      <textarea style={styles[changeValue]} />
       <div className="my-3">{colorBoxes}</div>
     </div>
   );
